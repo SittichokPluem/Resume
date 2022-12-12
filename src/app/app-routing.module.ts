760fa895d35b731041a8e6f16f4b2fdf5387c91c
SittichokPluem <Sittichokpluem@gmail.com>
2022-12-12 10:37:18 +0700

    12

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -17,7 +17,7 @@ const routes: Routes = [
     {path:'skills',component:SkillsComponent},
     {path:'education',component:EducationComponent},
     {path:'contact',component:ContactComponent},
-   
+  
    
   {path:'header-component',component:HeaderComponent}
   
