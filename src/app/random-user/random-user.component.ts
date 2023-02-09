360d037228d1f5a543548c2f669ba5b884b886b5
SittichokPluem <Sittichokpluem@gmail.com>
2023-02-09 18:11:45 +0700

    random user api

--- src/app/random-user/random-user.component.ts
+++ src/app/random-user/random-user.component.ts
@@ -9,20 +9,33 @@ import { throwError } from 'rxjs';
   styleUrls: ['./random-user.component.scss']
 })
 export class RandomUserComponent implements OnInit {
-  user: any;
+  user1: any;
+user2: any;
+user3: any;
   error: string | undefined;
   constructor(private http: HttpClient) { }
-
   ngOnInit() {
-    this.http.get('https://random-data-api.com/api/users/random_user')
+    
+    this.getRandomUser().then(data => {
+      this.user1 = data;
+      this.getRandomUser().then(data => {
+        this.user2 = data;
+        this.getRandomUser().then(data => {
+          this.user3 = data;
+        });
+      });
+    });
+  }
+
+  async getRandomUser() {
+    return await this.http.get('https://random-data-api.com/api/users/random_user')
       .pipe(
         catchError((error: HttpErrorResponse) => {
           this.error = error.message;
           return throwError(error);
         })
       )
-      .subscribe(data => {
-        this.user = data;
-      });
+      .toPromise();
   }
-}
+  }
+
